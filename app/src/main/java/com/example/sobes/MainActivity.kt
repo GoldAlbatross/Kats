0e2d6f80ce871e22614b751de27a5804380f8965
GoldAlbatross <snovaodin@gmail.com>
2023-02-18 15:50:43 +0300

    do not touch! You can't stop scrolling this:)

--- app/src/main/java/com/example/sobes/MainActivity.kt
+++ app/src/main/java/com/example/sobes/MainActivity.kt
@@ -2,9 +2,13 @@ package com.example.sobes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.RoundedCorner
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.example.sobes.okhttp.Cat
 import com.example.sobes.okhttp.CatApi
 import okhttp3.OkHttpClient
@@ -16,9 +20,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
-
-    lateinit var recyler: RecyclerView
-    private var catAdapter = KatAdapter { showCat() }
+    lateinit var img: ImageView
 
     private val baseUrl = "https://api.thecatapi.com/"
     private val logging = HttpLoggingInterceptor().apply {
@@ -34,17 +36,19 @@ class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        recyler = findViewById(R.id.recycler)
-        recyler.layoutManager = LinearLayoutManager(this)
+        setContentView(R.layout.image_box)
+        img = findViewById(R.id.img_kitties)
+        img.setOnClickListener { showCat() }
 
         catService.getCats().enqueue(object : Callback<List<Cat>>{
             override fun onResponse(call: Call<List<Cat>>, response: Response<List<Cat>>) {
                 if (response.code() == 200) {
-                    catAdapter.urls.add(response.body()?.get(0)?.url.toString())
-                    recyler.adapter = catAdapter
-                    recyler.adapter?.notifyDataSetChanged()
+                    val x = response.body()?.get(0)?.url.toString()
+                    Glide.with(this@MainActivity)
+                        .load(x)
+                        .centerCrop()
+                        .transform(RoundedCorners(60))
+                        .into(img)
                 }
             }
             override fun onFailure(call: Call<List<Cat>>, t: Throwable) {
@@ -55,9 +59,12 @@ class MainActivity : AppCompatActivity() {
         catService.getCats().enqueue(object : Callback<List<Cat>>{
             override fun onResponse(call: Call<List<Cat>>, response: Response<List<Cat>>) {
                 if (response.code() == 200) {
-                    catAdapter.urls.add(response.body()?.get(0)?.url.toString())
-                    recyler.adapter = catAdapter
-                    recyler.adapter?.notifyDataSetChanged()
+                    val x = response.body()?.get(0)?.url.toString()
+                    Glide.with(this@MainActivity)
+                        .load(x)
+                        .centerCrop()
+                        .transform(RoundedCorners(60))
+                        .into(img)
                 }
             }
             override fun onFailure(call: Call<List<Cat>>, t: Throwable) {
